99ef5bb759b09de2b70d39198cddf77391186090
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-14 09:47:05 -0400

    Create header.php
    May have issues with the includes due to folders. Easy fix if there is an issue.

---
+++ site-structure/header.php
@@ -0,0 +1,9 @@
+<? include ("top.php"); ?>
+
+<header>
+    <a href="http://www.potsdam.ny.us/" target="_blank"><img src="Village_Logo.png" id="logo" alt="Potdsam"></a>
+    <h1>Potsdam, NY</h1>
+</header>
+
+
+<? include ("nav.php"); ?>
