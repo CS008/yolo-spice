bf7afaf7229b45aeed378ac86085e1c4903a9acb
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-14 09:45:34 -0400

    Create footer.php

---
+++ site-structure/footer.php
@@ -0,0 +1,12 @@
+<footer>
+	<p>Site Design by: <a href="mailto:mdfritz@uvm.edu" target="_blank">Michael Fritz</a> </p>
+
+<?php
+	// outputs e.g. 'Last modified: March 04 1998 20:43:59.'
+	echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());
+?>
+
+</footer>
+
+</body>
+</html>
