2796afa33817d55894f463fc68dc7001394ac7cc
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-16 13:06:21 -0400

    Update header.php

--- site-structure/header.php
+++ site-structure/header.php
@@ -1,8 +1,7 @@
 <? include ("top.php"); ?>
 
 <header>
-    <a href="http://www.potsdam.ny.us/" target="_blank"><img src="Village_Logo.png" id="logo" alt="Potdsam"></a>
-    <h1>Potsdam, NY</h1>
+    <h1>Insert a site name here!</h1>
 </header>
 
 
