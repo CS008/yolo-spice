47f47d23b28155dfbf8f40a360659b11e0138dea
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-16 13:00:18 -0400

    Update nav.php

--- site-structure/nav.php
+++ site-structure/nav.php
@@ -7,12 +7,33 @@
         else {
             print '<li><a href="home.php">Home</a></li>';
         }
-
-        if ($path_parts['filename'] == "form") {
-            print '<li class="activePage">Form</li>';
+        
+        if ($path_parts['filename'] == "adrian") {
+            print '<li class="activePage">Adrian\'s Reviews</li>';
         }
         else {
-            print '<li><a href="form.php">Form</a></li>';
+            print '<li><a href="adrian.php">Adrian\'s Reviews</a></li>';
+        }
+        
+        if ($path_parts['filename'] == "bill") {
+            print '<li class="activePage">Bill\'s Reviews</li>';
+        }
+        else {
+            print '<li><a href="bill.php">Bill\'s Reviews</a></li>';
+        }
+        
+        if ($path_parts['filename'] == "mike") {
+            print '<li class="activePage">Michael\'s Reviews</li>';
+        }
+        else {
+            print '<li><a href="mike.php">Michael\'s Review</a></li>';
+        }
+        
+        if ($path_parts['filename'] == "contact-us") {
+            print '<li class="activePage">Contact Us!</li>';
+        }
+        else {
+            print '<li><a href="contact-us.php">Contact Us!</a></li>';
         }
 
         ?>
