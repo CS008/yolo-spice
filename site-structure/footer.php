804cb6253b2657d975fd5abb61d02d52bfe6fa40
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-16 12:57:40 -0400

    Update footer.php

--- site-structure/footer.php
+++ site-structure/footer.php
@@ -1,9 +1,9 @@
 <footer>
-	<p>Site Design by: <a href="mailto:mdfritz@uvm.edu" target="_blank">Michael Fritz</a> </p>
+	<p>Site Design by: <a href="mailto:mdfritz@uvm.edu" target="_blank">Yolo-Spice</a> </p>
 
 <?php
 	// outputs e.g. 'Last modified: March 04 1998 20:43:59.'
-	echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());
+	echo "<p>Last modified: " . date ("F d Y H:i:s.", getlastmod()) "</p>";
 ?>
 
 </footer>
