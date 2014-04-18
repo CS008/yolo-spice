e74bd36cd010b26ba2de0131bd952f6557fc4fbb
Bill Matraw <Bahldar@users.noreply.github.com>
2014-04-18 14:12:44 -0400

    Create bill.php

---
+++ bill.php
@@ -0,0 +1,65 @@
+<body id=bill>
+	<h1>Bill reviews Northside Cafe</h1>
+	<h4>located on Trinity campus</h4>
+	<article class=review>
+		<h2>Northside Special</h2>
+		<h3>General tao's chicken with jasmine rice and green beans</h3>
+		<p>
+			First off the green beans. They weren't too crunchy and they fell more towards the mushy side. Most likely to 
+			have been because it sat around for a while.		
+		</p>
+		<p>
+			Next was the chicken. Precooked in its entirety. overall it was a sub-par gum flavour, as it was quite chewy.
+		</p>
+		<p>
+		The Rice by itself was a bit bland, which is to be expected with plain white rice. However, mixed with the chicken
+		the combined textures was very pleasing.
+		</p>
+		<p>
+		Last but certainly not least was the sauce. A mixture of tangy sweet and sour complimented by the red pepper gives it a nice kick.
+		
+		</p>
+		<p>
+		Overall this dish receives 3.5 Rainbows
+		</p>
+		
+	</article>
+	<article class = review>
+		<h2>Northside Pizza</h2>
+		<h3>Meatlover's special<h3>
+		<p>
+			This pizza should have been confiscated by BP because this pizza had an excruciating amount of oil on it.
+		</p>
+		<p>
+			The individual meats were of lesser quality as to be expected from sodexo.	First off is the sausage: It was flavourful enough, but upon closer inspection
+			had chunks of fat. The peperoni was very crispy and acceptable. The ham seemed to have been sliced thinly off of a ham steak of decent quality. Lastly was the bacon.
+			I didn't even realize there was bacon at first, I just thought they had burned part of the pizza. They would have better off laying the bacon under the cheese to preserve
+			its flavour. There also was a sub-par sprinkling of spices on the pizza after it had cooked. It would have been loads better if they had sprinkled it on before they cooked. 
+			That way the flavours would have time to extend its reach
+		
+		</p>
+		<p>
+		Overall the oily mess of a pizza gets 2.0 Rainbows
+		</p>
+	
+	
+	</article>
+	
+	<article class = review>
+		<h2>Northside Dessert</h2>
+		<h3>Turtle Brownie from Redstone bakery</h3>
+		<p>
+			This thing is really god damn chewy. It doesn't even deserve to be called a brownie. More like chocolate flavoured chewing gum.
+		</p>
+		<p>
+			Still its a brownie, 3.0 Rainbows
+		</p>
+		
+		
+	
+	
+	</article>
+	
+
+
+</body>
