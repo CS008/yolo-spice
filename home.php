ad774e45fe3630e51b20b14e4e1bb569bca481f0
Michael Fritz <fritzles@users.noreply.github.com>
2014-04-24 21:54:02 -0400

    Update home.php
    Add content, give clear direction as to what's next.

--- home.php
+++ home.php
@@ -2,7 +2,13 @@
 
 <article class="main">
 
-	<h1> Stuff </h1>
+	<h1> Welcome to the UDS* Review Page!</h1>
+	
+	<p> So, you're new here at UVM, are you? Sodexo food been toted as many things. The Grundle is the "worst" place to eat? Is Cook delicious? Here is where you can find all the honest reviews done by our staff. </p>
+	
+	<h2> Meet our Staff </h2>
+	
+	<!-- Insert Table/other form of presenting Adrian, Bill, and Mike. -->
 
 </article>
 
