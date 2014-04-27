f9faff1ef391cda7be46d4d9460f720ab11ff36e
Bahldar <wmatraw@okapi.uvm.edu>
2014-04-27 16:59:39 -0400

    Testers

--- site-structure/footer.php
+++ site-structure/footer.php
@@ -3,7 +3,7 @@
 
 <?php
 	// outputs e.g. 'Last modified: March 04 1998 20:43:59.'
-	echo "<p>Last modified: " . date ("F d Y H:i:s.", getlastmod()) "</p>";
+	echo "<p>Last modified: " . date ("F d Y H:i:s.", getlastmod()) . "</p>";
 ?>
 
 </footer>
