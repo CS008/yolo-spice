f7dff4e67553b386a2251dd856ca8b03b0a716ea
Bahldar <wmatraw@okapi.uvm.edu>
2014-04-28 12:03:38 -0400

    Finished up my Comments pagegit add .git add .~~~~~~

---
+++ lib/security.php
@@ -0,0 +1,21 @@
+<?php
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//performs a few security checks
+function securityCheck($form = "") {
+    
+    // globals defined in top.php
+    global $yourURL;
+
+    $status = true; // start off thinking everything is good until a test fails
+    $fromPage = htmlentities($_SERVER['HTTP_REFERER'], ENT_QUOTES, "UTF-8"); 
+    // $frompage = getnev("https_referer");
+    if ($debug) print "<p>From: " . $fromPage . " should match your Url: " . $yourURL;
+
+    if($fromPage != $yourURL){
+        $status=false;
+        
+    } 
+    
+    return $status;
+}
+?>
---
+++ lib/validationFunctions.php
@@ -0,0 +1,36 @@
+<?php
+
+function verifyAlphaNum($testString) {
+    // Check for letters, numbers and dash, period, space and single quote only.
+    return (preg_match("/^([[:alnum:]]|-|\.| |')+$/", $testString));
+}
+
+function verifyEmail($testString) {
+    // Check for a valid email address
+    return filter_var($testString, FILTER_VALIDATE_EMAIL);
+}
+
+function verifyText($testString) {
+    // Check for letters, numbers and dash, period, ?, !, space and single and double quotes only.
+    return (preg_match("/^([[:alnum:]]|-|\.| |\n|\r|\?|\!|\"|\')+$/", $testString));
+}
+
+function verifyPhone($testString) {
+    // Check for only numbers, dashes and spaces in the phone number
+    return (preg_match('/^([[:digit:]]| |-)+$/', $testString));
+}
+
+function validNumber($testString) {
+    return is_numeric($testString);
+}
+
+function validInteger($testString) {
+    return filter_var($testString , FILTER_VALIDATE_INT);
+}
+
+function verifyName($testString){
+    return (preg_match("^[A-Za-z]+$^", $testString));
+}
+
+
+?>
