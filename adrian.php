a706cf8006912fdaf2ab37ad1e5603aed3a8571e
apascual3435 <apascual@uvm.edu>
2014-05-04 15:06:43 -0400

    Update adrian.php

--- adrian.php
+++ adrian.php
@@ -1,33 +1,51 @@
-<? include ("site-structure/header.php") ?>
+<body id=adrian>
+	
+    <h1>Adrian reviews Harris Mills Unlimited Dining (The Grundle)</h1>
+	<h4>located on Athletic Campus</h4>
+	<article class=review>
+		
+            
+ <h2> The Grundle Special</h2>
+    <h3>Glazed Pollock Marinated in a Seafood BBQ Sauce</h3>
+		
+      <p>Wow. Just wow. This was just a disaster. I only subjected myself to one
+      measly bite of this fishy atrocity, and it was still brutal. That fish must
+      have been sitting there for weeks. It was tangy, dry, and relentless. Avoid
+      this dish at all costs. I could not justify giving this fish any amount of
+      rainbows.</p>
+                <p>
+		NO RAINBOWS FOR YOU. 
+		</p>
 
-	<h1> Adrian Reviews ????? </h1>
+	</article>
+	<article class = review>
+		
+      <h2>The Grundle Pizza</h2>
+        <h3>Pepperoni Pizza<h3>
+		
+      <p>This pepperoni pizza was by far the worst I've had on campus. It was 
+      wayyyyy to chewy; it's texture was similar to that of a rubber band soaked
+      in gasoline. I would hope that no one ever subjects themselves to a 
+      sub-par slice. </p>
+		
 
-	<article class="review">
+      <p> 1.5 Rainbows</p>
 
-		<h2> Thing I ate </h2>
-
-			<p> words about the thing i ate </p>
-
-			<p> more words about the thing I ate </p>
-
-			<p> rating goes here </p>
-
-		<h2> other thing I ate </h2>
-
-			<p> words words words </p>
-
-			<p> wordssssss </p>
-
-			<p> other words </p>
-
-		<h2> last thing i ate </h2>
-
-			<p> asdgdsg </p>
-
-			<p> DSGQH34Q3HB	</p>
-
-			<p> rainbows </p>
 
 	</article>
 
-<? include("site-structure/footer.php") ?>
+<article class = review>
+		<h2>The Grundle Dessert</h2>
+		<h3>Chocolate Cake with Vanilla Icing</h3>
+
+ <p>This cake was quite delicious. The cake itself was very spongy and light while
+ the vanilla frosting overpowered much of the chocolate. It was pure sugar
+ ecstacy and I would recommend it to anyone with a love of sweets.</p>
+		
+ <p>
+			Totally solid 4/5 Rainbows
+		</p>
+	
+    </article>
+
+</body>
