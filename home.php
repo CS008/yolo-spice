177c8af50818e622197eb207d3690da1070e2c22
fritzles <mdfritz@uvm.edu>
2014-05-04 22:30:40 -0400

    more things

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
 <article class="main">
 
 	<h1> Welcome to the UDS* Review Page!</h1>
+
+	<img src="https://uds.uvm.edu/images/locations/uvm_location_map.gif" class="map">
 	
 	<p> So, you're new here at UVM, are you? Sodexo food been toted as many things. The Grundle is the "worst" place to eat? Is Cook delicious? Here is where you can find all the honest reviews done by our staff. </p>
 
--- mike.php
+++ mike.php
@@ -2,19 +2,10 @@
 
 	<h1> Michael Reviews Cook Commons Dining </h1>
 
-	<article class="review">
-
-		<h2> Chef's Salad Wrap </h2>
-
-			<p> In the center of Cook Dining, there is a nice station that holds the salad bar, soups, and specialty wrap of the day. All in all, the speicalty wraps are very delicious, rather nutricious, and always a delight. This week, I was lucky enough to try a classic: The Chef's Salad Wrap. </p>
-
-			<p> The wrap conainted slices of ham, turkey, cheddar cheese, lettuce, tomatoes, and a very light ranch dressing. Combined, it was a very satisfying meal. There is no way you could go wrong with this selection. </p>
-
-			<p> I know why you're all here: The ratings! Well, for this particular dish, I'd rate it as 3.5/5 Rainbows. It was delicious and satisfying, but the wrap wasn't creative. My mindset is that when one visits the specialty wrap station, one must be amazed by how delicious the cobmination is and how exotic it is. </p>
-<img class="35-rainbow" src="pics/rainbow.png" alt="" height="14" width="0">
-			</article>
-			<article class = review>
+	<article class = "review">
 		<h2> Grilled Chicken Sandwich and Fries </h2>
+
+		<img src="http://4.bp.blogspot.com/-o4Tl6aJwo3Q/T6yJFODgOOI/AAAAAAAAAfk/w1oXVoU1BDU/s1600/gluten+free+chicken+sandwich+with+gluten+free+fries.jpg" height="40" width="50" class="pic">
 
 			<p> Another staple of Cook Dining is the grill. Their hamburgers, grilled cheese, and other classics are a staple of Cook. Plus, every sandwhich can be accompainied by a nice helping of fries. </p>
 
@@ -24,14 +15,34 @@
 
 	<img class="40-rainbow" src="pics/rainbow.png" alt="" height="14" width="0">		
 			</article>
-			<article class = review>
+
+	<article class="review">
+
+		<h2> Cheese Pizza </h2>
+
+		<img src="http://3.bp.blogspot.com/-khLBBy18WL0/UVy_YJZ4MwI/AAAAAAAAggw/xdSl3g4B-J4/s1600/Sheet+Pan+Pizza+-+Missing+Slice.JPG" height="40" width="50" class="pic">
+
+			<p> In the center of Cook Dining, there is a nice station that holds the salad bar, soups, and specialty wrap of the day. All in all, the speicalty wraps are very delicious, rather nutricious, and always a delight. This week, I was lucky enough to try a classic: The Chef's Salad Wrap. </p>
+
+			<p> The wrap conainted slices of ham, turkey, cheddar cheese, lettuce, tomatoes, and a very light ranch dressing. Combined, it was a very satisfying meal. There is no way you could go wrong with this selection. </p>
+
+			<p> I know why you're all here: The ratings! Well, for this particular dish, I'd rate it as 3.5/5 Rainbows. It was delicious and satisfying, but the wrap wasn't creative. My mindset is that when one visits the specialty wrap station, one must be amazed by how delicious the cobmination is and how exotic it is. </p>
+<img class="35-rainbow" src="pics/rainbow.png" alt="" height="14" width="0">
+			</article>
+			
+			<article class = "review">
 		<h2> Cook's Ice Cream  </h2>
+
+		<img src="http://1.bp.blogspot.com/_rJCsXH6KLTk/SmOQAw8YJlI/AAAAAAAAEs4/Uh9h3EDlxhg/s400/blueicecream2.jpg" height="40" width="50" class="pic">
+
 
 			<p> At Cook, the one desert you cannot go wrong with is the ice cream. With six flavors, it has the most of any unlimited dining location. Their flavors vary from day to day (except Saturday becuase they're closed) and they never dissapoint.  </p>
 
 			<p> Their ice cream flavors, specifically the dinosaur named one (I can never remember it. Also, it's blue. WALTER WHITE) is the most tantalizing. If you're a fan traditional ice cream, be ready to be amazed. Fan of trying new things? Then try Dinosaur Crunch! I highly reccomend you grab a cone of this. </p>
 
 			<p> You can never go wrong with ice cream. 5.0/5 Rainbows for Dinosaur Crunch! </p>
+
+			
 
 	<img class="50-rainbow" src="pics/rainbow.png" alt="" height="14" width="0">		
 	</article>
