a42102e4c6738f426ac648ec9dadaf63bbd38047
apascual3435 <apascual@uvm.edu>
2014-05-04 20:20:42 -0400

    Update security.php

--- lib/security.php
+++ lib/security.php
@@ -1,17 +1,16 @@
 <?php
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //performs a few security checks
-function securityCheck() {
+function securityCheck($form = "") {
     
     // globals defined in top.php
     global $yourURL;
 
     $status = true; // start off thinking everything is good until a test fails
     $fromPage = htmlentities($_SERVER['HTTP_REFERER'], ENT_QUOTES, "UTF-8"); 
-    // $frompage = getnev("https_referer");
 
-    if ($debug) print "<p>From: " . $fromPage . " should match your Url: " . $yourURL;
-
+    if ($debug) print "<p>From: " . $fromPage . " should match yourUrl: " . $yourURL;
 
     if($fromPage != $yourURL){
         $status=false;
