da37d417f952a5d97fe10c1e1f762a11fd60f49a
fritzles <mdfritz@uvm.edu>
2014-05-05 00:06:32 -0400

    CSS VALIDATION FLAIR

--- site-structure/footer.php
+++ site-structure/footer.php
@@ -6,6 +6,14 @@
 	echo "<p>Last modified: " . date ("F d Y H:i:s.", getlastmod()) . "</p>";
 ?>
 
+<p class = "left">
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+</a>
+</p>
+
 </footer>
 
 </body>
