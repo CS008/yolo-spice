56f2b22b218409941e91aebc7323909ca518cb92
fritzles <mdfritz@uvm.edu>
2014-05-04 23:46:03 -0400

    oh my god 150 words blows

--- home.php
+++ home.php
@@ -8,6 +8,9 @@
 	
 	<p> So, you're new here at UVM, are you? Sodexo food been toted as many things. The Grundle is the "worst" place to eat? Is Cook delicious? Here is where you can find all the honest reviews done by our staff. </p>
 
+	<p> Well, all of your questions are about to be answered. From here on, this site will give you a quick guide to campus food and everything you can expect. Bill, Adrian, and Michael have all worked tirelessly to bring you the hard truth. And, we have pictures! So, dive on in and find out what've you need to know to make campus life nice and easy when it comes to dining.</p>
+
+	<p> Reviews Available: Cook Commons Unlimited, Northside, Harris Millis Unlimited. If you'd like to see another location added to our list, don't hesitate to contact us! And, feel free to leave comments on our comments page. Enjoy and "Be Cool."</p>
 </article>
 
 
