03167782e38220bdb7361582ee7b5ac28cf44438
fritzles <mdfritz@uvm.edu>
2014-05-04 22:08:51 -0400

    nav bargit add site-structure/nav.php git add site-structure/nav.php  yay

--- site-structure/nav.php
+++ site-structure/nav.php
@@ -1,48 +1,49 @@
+<div class="horizontal">
 <nav>
     <ol>
         <?php
         if ($path_parts['filename'] == "home") {
-            print '<li class="activePage">Home</li>';
+            print '<li><a href="home.php">Home Page</a></li>';
         }
         else {
-            print '<li><a href="home.php">Home</a></li>';
+            print '<li><a href="home.php">Home Page</a></li>';
         }
         
         if ($path_parts['filename'] == "adrian") {
-            print '<li class="activePage">Adrian\'s Reviews</li>';
+            print '<li><a href="adrian.php">Adrian\'s Reviews</a></li>';
         }
         else {
             print '<li><a href="adrian.php">Adrian\'s Reviews</a></li>';
         }
         
         if ($path_parts['filename'] == "bill") {
-            print '<li class="activePage">Bill\'s Reviews</li>';
+            print '<li><a href="bill.php">Bill\'s Reviews</a></li>';
         }
         else {
             print '<li><a href="bill.php">Bill\'s Reviews</a></li>';
         }
         
         if ($path_parts['filename'] == "mike") {
-            print '<li class="activePage">Michael\'s Reviews</li>';
+            print '<li><a href="mike.php">Michael\'s Review</a></li>';
         }
         else {
             print '<li><a href="mike.php">Michael\'s Review</a></li>';
         }
         
         if ($path_parts['filename'] == "contact-us") {
-            print '<li class="activePage">Contact Us!</li>';
-        }
+print '<li><a href="contact-us.php">Contact Us!</a></li>';        }
         else {
             print '<li><a href="contact-us.php">Contact Us!</a></li>';
         }
         
         if ($path_parts['filename'] == "comments") {
-            print '<li class="activePage">Leave a comment</li>';
+            print '<li><a href="comments.php">Comment Page</a></li>';
         }
         else {
-            print '<li><a href="comments.php">Comments</a></li>';
+            print '<li><a href="comments.php">Comment Page</a></li>';
         }
 
         ?>
     </ol>
 </nav>
+</div>
